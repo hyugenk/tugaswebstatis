8e8c7d1b5e0eb19e5da9391dcd1c008f0fcde1e4
hyugenk <89519700+hyugenk@users.noreply.github.com>
2022-01-05 00:20:06 +0700

    Update style.js

--- style.js
+++ style.js
@@ -1,4 +1,3 @@
-// Fungsi biar bisa login pake enter
 $("#passInput").keypress(function(event) {
     if (event.keyCode === 13) {
         $("#btnLogin").click();
@@ -11,14 +10,14 @@ function login() {
     var mail = document.getElementById("mailInput").value;
     var pass = document.getElementById("passInput").value;
     var salah = document.getElementById("slh");
-    const mailDb = "administrator"                                     // Email Admin
-    const passDb = "sudo123"                                          //  Password Admin
+    const mailDb = "admin"                                     // Email Admin
+    const passDb = "adminpass"                                 //  Password Admin
   
     console.log(mail)
     console.log(pass)
     salah.style.display = "none";
     if (mail === mailDb && pass === passDb) {
-      location.replace("/co.html")
+      location.replace("beranda.html")
     } else{
         salah.style.display = "block";
       }
